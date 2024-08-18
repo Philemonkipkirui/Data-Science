51dbfe1e7ef0e0f8f2d2163b01e87391b7d422d4
Philemonkipkirui <56266352+Philemonkipkirui@users.noreply.github.com>
2024-08-18 19:53:55 +0300

    Add files via upload

---
+++ Week1Script-1.sql
@@ -0,0 +1,17 @@
+select *
+from `1. weather data` wd 
+where `Weather` = 'Snow' and Visibility_km = '25' or `Rel Hum_%` > '50';
+
+
+select *
+from `1. weather data` wd 
+where `Wind Speed_km/h` = '4';
+
+
+select avg( Visibility_km ,) as mean_value
+from `1. weather data` wd 
+
+
+select *
+from `1. weather data` wd 
+where `Weather` = "Snow";
